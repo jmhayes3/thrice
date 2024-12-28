36e71ed76055042e0da6e06764b18930d4ae078b
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2024-12-28 14:39:30 -0600

    add tailwind built styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/**/*.{html,js}"
+    "./public/**/*.{html,js}",
+    "./public/index.html",
   ],
   theme: {
     extend: {},
