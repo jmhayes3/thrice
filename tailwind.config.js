9c7bfefd868f87c2caf486cb8a449d706c120928
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2024-12-29 20:51:47 -0600

    update config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/**/*.{html,js}",
-    "./public/index.html",
+    "./frontend/public/**/*.{html,js}",
   ],
   theme: {
     extend: {
