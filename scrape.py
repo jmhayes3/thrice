2c3a7f93398b78e6b542be453a40a2f3c73dbddb
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2024-12-31 18:48:52 -0600

    remove assertions

--- scrape.py
+++ scrape.py
@@ -80,8 +80,6 @@ def scrape(day, output_file=None):
             clues = extract_clues(selection)
             print("CLUES\n-------", clues)
 
-            # assert len(answers) == 5
-            # assert len(clues) == 5
             for clue in clues:
                 print(clue)
 
