5d7f87d6a7099d45a6a5da7990809861334ce7a1
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2025-01-04 16:35:01 -0600

    clean up files

--- backend/src/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function onRequest(context: EventContext<any, any, any>): Promise<Response>;
--- backend/src/index.js
+++
@@ -1,4 +0,0 @@
-export async function onRequest(context) {
-    return new Response("Hello, world!");
-}
-//# sourceMappingURL=index.js.map
