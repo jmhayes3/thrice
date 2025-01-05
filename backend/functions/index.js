17b8ecb9b21f20d106f6843271d911a04e314a7d
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2025-01-04 21:36:22 -0600

    d1 database

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,3 +1,4 @@
-export async function onRequestGet() {
-  return new Response("index.js -- Hello from Pages Functions!");
+export async function onRequestGet(context) {
+  const { results } = await context.env.DB.prepare("SELECT * FROM Customers WHERE CompanyName = ?",).bind("Bs Beverages").all();
+  return Response.json(results);
 }
