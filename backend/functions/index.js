c3814bc497bdadb7c20f292660af91a4a8c91f6e
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2025-01-04 23:10:08 -0600

    delete functions/index.js

--- backend/functions/index.js
+++
@@ -1,6 +0,0 @@
-// index.js
-
-export async function onRequestGet(context) {
-  const { results } = await context.env.DB.prepare("SELECT * FROM sensor_readings WHERE event_id = ?",).bind(1).all();
-  return Response.json(results);
-}
