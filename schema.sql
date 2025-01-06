b5e996f6e2e6c29755f933e39063b9801c92fb43
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2025-01-06 05:34:29 -0600

    add trailing comma

--- schema.sql
+++ schema.sql
@@ -24,7 +24,7 @@ CREATE TABLE clues (
     round_id INTEGER,
     clue_number INTEGER CHECK (clue_number BETWEEN 1 AND 3),
     clue_text TEXT NOT NULL,
-    percent_correct INTEGER CHECK (percent_correct BETWEEN 0 AND 100)
+    percent_correct INTEGER CHECK (percent_correct BETWEEN 0 AND 100),
     points INTEGER CHECK (points BETWEEN 1 AND 3),
     FOREIGN KEY (round_id) REFERENCES rounds(round_id),
     UNIQUE (round_id, clue_number)
