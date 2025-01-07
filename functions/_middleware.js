fc8aa138f6cd02de07940988643e427ac2ab6ebf
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2025-01-06 19:13:33 -0600

    simplify project structure

---
+++ functions/_middleware.js
@@ -0,0 +1,9 @@
+async function errorHandling(context) {
+  try {
+    return await context.next();
+  } catch (err) {
+    return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+  }
+}
+
+export const onRequest = [errorHandling];
