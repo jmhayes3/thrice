186acb142d7220d5c5c527cfb51cb33b864a30a8
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2025-01-29 06:23:11 -0600

    remove App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,34 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Input } from "@/components/ui/input";
+
+export function InputWithButton() {
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input type="text" placeholder="Answer here..." />
+      <Button type="submit">Submit</Button>
+    </div>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <Button variant="ghost" onClick={() => setCount((count) => count + 1)}>
+      <div>
+        <Button
+          variant="outline"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
-        <Button variant="outline">Click me!</Button>
-      </div>
+      <InputWithButton />
     </>
   );
 }
