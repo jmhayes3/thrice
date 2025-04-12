6d5993b16855f61589e8745bcd3c19540e02d000
jmhayes3 <22490346+jmhayes3@users.noreply.github.com>
2025-04-12 02:31:09 -0500

    rm _middleware.js

--- functions/_middleware.js
+++
@@ -1,9 +0,0 @@
-async function errorHandling(context) {
-  try {
-    return await context.next();
-  } catch (err) {
-    return new Response(`${err.message}\n${err.stack}`, { status: 500 });
-  }
-}
-
-export const onRequest = [errorHandling];
